db23495af7fddb50c0b7be64f0ac64b71cda2912
Kenzo Dewaegenaere <71668365+Kenzo-dewaegenaere@users.noreply.github.com>
2024-02-10 01:54:37 +0100

    Add @react-three/postprocessing package and apply pixelation effect to LandingScene

--- src/components/Home/LandingScene.tsx
+++ src/components/Home/LandingScene.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { Canvas  } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Model } from '../../../public/Hacker_room_low_poly.tsx';
+import { EffectComposer, Pixelation } from '@react-three/postprocessing';
+import * as THREE from 'three';
 
 const Home: React.FC = () => {
+  
   return (
     <>
-        <Canvas>
-        <OrbitControls />
-        <ambientLight />
-        <pointLight position={[100, 100, 100]} />
-
-
+      <Canvas camera={{ position: new THREE.Vector3(1.3, 1.6, 0.6),  }} >
+        <OrbitControls target={ new THREE.Vector3(-0.19289723849309587, 0.6851479096457078, -0.07967731899121745)} enableDamping={false} enablePan={false}  />
+        <ambientLight intensity={0.5} />
         <Model />
-        </Canvas>
+        <EffectComposer>
+          <Pixelation granularity={4} />
+        </EffectComposer>
+      </Canvas>
     </>
   );
 };
