8b24b0128bde1a7b7bbdb22ad8f7c4af1f69b7f1
Kenzo-dewaegenaere <71668365+Kenzo-dewaegenaere@users.noreply.github.com>
2024-02-11 19:39:11 +0100

    Refactor camera and lighting in LandingScene component to use typed constants

--- src/components/Home/LandingScene.tsx
+++ src/components/Home/LandingScene.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Canvas  } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { Model } from '../../../public/Hacker_room_low_poly.tsx';
 import { EffectComposer, Pixelation } from '@react-three/postprocessing';
 import * as THREE from 'three';
+
+const CAMERA_POSITION: [number, number, number] = [0.5, 0.8, 0.6];
+const TARGET_POSITION: THREE.Vector3 = new THREE.Vector3(-0.0689723849309587, 0.5851479096457078, 0.131899121745);
+const AMBIENT_LIGHT_INTENSITY: number = 0.5;
+const PIXELATION_GRANULARITY: number = 3;
+const FOV: number = 75;
 
 const Home: React.FC = () => {
   return (
@@ -16,16 +22,15 @@ const Home: React.FC = () => {
 };
 
 const Scene: React.FC = () => {
-  
   return (
     <>
-        <PerspectiveCamera makeDefault position={[.5, .8, .6]} fov={75} />
-        <OrbitControls target={ new THREE.Vector3(-0.0689723849309587, 0.5851479096457078, .131899121745)} enableDamping={false} enablePan={false}  />
-        <ambientLight intensity={0.5} />
-        <Model />
-        <EffectComposer>
-          <Pixelation granularity={3} />
-        </EffectComposer>
+      <PerspectiveCamera makeDefault position={CAMERA_POSITION} fov={FOV} />
+      <OrbitControls target={TARGET_POSITION} enableDamping={false} enablePan={false} />
+      <ambientLight intensity={AMBIENT_LIGHT_INTENSITY} />
+      <Model />
+      <EffectComposer>
+        <Pixelation granularity={PIXELATION_GRANULARITY} />
+      </EffectComposer>
     </>
   );
 };
