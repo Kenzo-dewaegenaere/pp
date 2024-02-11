7ec494b333fd1a9805c526b5f666e813c3325ffc
Kenzo-dewaegenaere <71668365+Kenzo-dewaegenaere@users.noreply.github.com>
2024-02-11 19:37:29 +0100

    Add PerspectiveCamera to LandingScene

--- src/components/Home/LandingScene.tsx
+++ src/components/Home/LandingScene.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import { Canvas  } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { Model } from '../../../public/Hacker_room_low_poly.tsx';
 import { EffectComposer, Pixelation } from '@react-three/postprocessing';
 import * as THREE from 'three';
 
 const Home: React.FC = () => {
+  return (
+    <>
+      <Canvas>
+        <Scene />
+      </Canvas>
+    </>
+  );
+};
+
+const Scene: React.FC = () => {
   
   return (
     <>
-      <Canvas camera={{ position: [.5, .8, .6]  }} >
+        <PerspectiveCamera makeDefault position={[.5, .8, .6]} fov={75} />
         <OrbitControls target={ new THREE.Vector3(-0.0689723849309587, 0.5851479096457078, .131899121745)} enableDamping={false} enablePan={false}  />
         <ambientLight intensity={0.5} />
         <Model />
         <EffectComposer>
           <Pixelation granularity={3} />
         </EffectComposer>
-      </Canvas>
     </>
   );
 };
